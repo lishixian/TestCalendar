190364b48ef9d2e0c6b8e0a7cb69b3db07857f63
lishixian <lishixian@le.com>
2018-04-20 17:30:52 +0800

    导入控件并成功显示

--- app/src/main/java/com/tea/testcalendar/MainActivity.java
+++ app/src/main/java/com/tea/testcalendar/MainActivity.java
@@ -7,11 +7,11 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.Window;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView mTextMessage;
     private Fragment[] fragments;
     private int lastShowFragment = 0;
 
@@ -26,21 +26,18 @@ public class MainActivity extends AppCompatActivity {
                         switchFrament(lastShowFragment, 0);
                         lastShowFragment = 0;
                     }
-                    mTextMessage.setText(R.string.title_home);
                     return true;
                 case R.id.navigation_dashboard:
                     if (lastShowFragment != 1) {
                         switchFrament(lastShowFragment, 1);
                         lastShowFragment = 1;
                     }
-                    mTextMessage.setText(R.string.title_dashboard);
                     return true;
                 case R.id.navigation_notifications:
                     if (lastShowFragment != 2) {
                         switchFrament(lastShowFragment, 2);
                         lastShowFragment = 2;
                     }
-                    mTextMessage.setText(R.string.title_notifications);
                     return true;
             }
             return false;
@@ -51,9 +48,9 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
 
@@ -61,7 +58,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void initFragments(){
-        Fragment homeFragment = new Fragment();
+        Fragment homeFragment = new CalendarFragment();
         Fragment dashboardFragment = new Fragment();
         Fragment notificationFragment = new Fragment();
 
@@ -92,5 +89,6 @@ public class MainActivity extends AppCompatActivity {
 
         transaction.show(fragments[index]).commitAllowingStateLoss();
     }
+
 
 }
