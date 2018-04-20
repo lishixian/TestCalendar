79a94c313f7ceadf794c48b14cabb3390663299b
lishixian <lishixian@le.com>
2018-04-20 15:10:52 +0800

    添加联动的fragment

--- app/src/main/java/com/tea/testcalendar/MainActivity.java
+++ app/src/main/java/com/tea/testcalendar/MainActivity.java
@@ -3,6 +3,8 @@ package com.tea.testcalendar;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
@@ -10,6 +12,8 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
     private TextView mTextMessage;
+    private Fragment[] fragments;
+    private int lastShowFragment = 0;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -18,18 +22,31 @@ public class MainActivity extends AppCompatActivity {
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_home:
+                    if (lastShowFragment != 0) {
+                        switchFrament(lastShowFragment, 0);
+                        lastShowFragment = 0;
+                    }
                     mTextMessage.setText(R.string.title_home);
                     return true;
                 case R.id.navigation_dashboard:
+                    if (lastShowFragment != 1) {
+                        switchFrament(lastShowFragment, 1);
+                        lastShowFragment = 1;
+                    }
                     mTextMessage.setText(R.string.title_dashboard);
                     return true;
                 case R.id.navigation_notifications:
+                    if (lastShowFragment != 2) {
+                        switchFrament(lastShowFragment, 2);
+                        lastShowFragment = 2;
+                    }
                     mTextMessage.setText(R.string.title_notifications);
                     return true;
             }
             return false;
         }
     };
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,6 +56,41 @@ public class MainActivity extends AppCompatActivity {
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        initFragments();
+    }
+
+    private void initFragments(){
+        Fragment homeFragment = new Fragment();
+        Fragment dashboardFragment = new Fragment();
+        Fragment notificationFragment = new Fragment();
+
+        fragments = new Fragment[]{homeFragment, dashboardFragment, notificationFragment};
+        lastShowFragment = 0;
+        getSupportFragmentManager()
+                .beginTransaction()
+                .add(R.id.content, homeFragment)
+                .show(homeFragment)
+                .commit();
+
+    }
+
+    /**
+     * 切换Fragment
+     *
+     * @param lastIndex 上个显示Fragment的索引
+     * @param index     需要显示的Fragment的索引
+     */
+    public void switchFrament(int lastIndex, int index) {
+
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.hide(fragments[lastIndex]);
+
+        if (!fragments[index].isAdded()) {
+            transaction.add(R.id.content, fragments[index]);
+        }
+
+        transaction.show(fragments[index]).commitAllowingStateLoss();
     }
 
 }
